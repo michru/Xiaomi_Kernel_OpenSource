a2b1e9f6d8554da6025ce303804113958dd4197f
Nathan Chancellor <natechancellor@gmail.com>
2018-07-06 20:40:26 -0700

    Merge 4.9.101 into polaris-o-oss
    Changes in 4.9.101: (36 commits)
            8139too: Use disable_irq_nosync() in rtl8139_poll_controller()
            bridge: check iface upper dev when setting master via ioctl
            dccp: fix tasklet usage
            ipv4: fix memory leaks in udp_sendmsg, ping_v4_sendmsg
            llc: better deal with too small mtu
            net: ethernet: sun: niu set correct packet size in skb
            net: ethernet: ti: cpsw: fix packet leaking in dual_mac mode
            net/mlx4_en: Verify coalescing parameters are in range
            net/mlx5: E-Switch, Include VF RDMA stats in vport statistics
            net_sched: fq: take care of throttled flows before reuse
            net: support compat 64-bit time in {s,g}etsockopt
            openvswitch: Don't swap table in nlattr_set() after OVS_ATTR_NESTED is found
            qmi_wwan: do not steal interfaces from class drivers
            r8169: fix powering up RTL8168h
            sctp: handle two v4 addrs comparison in sctp_inet6_cmp_addr
            sctp: remove sctp_chunk_put from fail_mark err path in sctp_ulpevent_make_rcvmsg
            sctp: use the old asoc when making the cookie-ack chunk in dupcook_d
            tcp_bbr: fix to zero idle_restart only upon S/ACKed data
            tg3: Fix vunmap() BUG_ON() triggered from tg3_free_consistent().
            bonding: do not allow rlb updates to invalid mac
            net/mlx5: Avoid cleaning flow steering table twice during error flow
            bonding: send learning packets for vlans on slave
            tcp: ignore Fast Open on repair mode
            sctp: fix the issue that the cookie-ack with auth can't get processed
            sctp: delay the authentication for the duplicated cookie-echo chunk
            serial: sccnxp: Fix error handling in sccnxp_probe()
            futex: Remove duplicated code and fix undefined behaviour
            xfrm: fix xfrm_do_migrate() with AEAD e.g(AES-GCM)
            lockd: lost rollback of set_grace_period() in lockd_down_net()
            Revert "ARM: dts: imx6qdl-wandboard: Fix audio channel swap"
            l2tp: revert "l2tp: fix missing print session offset info"
            nfp: TX time stamp packets before HW doorbell is rung
            proc: do not access cmdline nor environ from file-backed areas
            futex: futex_wake_op, fix sign_extend32 sign bits
            kernel/exit.c: avoid undefined behaviour when calling wait4()
            Linux 4.9.101
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- arch/arm64/include/asm/futex.h
+++ arch/arm64/include/asm/futex.h
@@ -51,7 +51,6 @@ static inline int
 arch_futex_atomic_op_inuser(int op, int oparg, int *oval, u32 __user *uaddr)
 {
 	int oldval = 0, ret, tmp;
-	u32 __user *uaddr = __uaccess_mask_ptr(_uaddr);
 
 	pagefault_disable();
 
